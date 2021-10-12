9dfa0c4b7423a55a662950ce125b650cfe834342
Phan Anh <aviet434@gmail.com>
2021-10-12 16:39:21 +0700

    password for db:seed => 123

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$Onhhm0tAxboY8Z7SLVB8l.omXrilUqgaAbWNnDOW9BJeN5kUTz0BC', // 123
             'remember_token' => Str::random(10),
         ];
     }
